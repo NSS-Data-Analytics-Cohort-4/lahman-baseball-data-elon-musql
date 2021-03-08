6e3b3666813b5cbd12b6bca739f7821d3517174a
brendagutman <brenda.gutman@gmail.com>
2021-03-08 08:39:45 -0600

    q6

--- data/Brenda_answers.sql
+++ data/Brenda_answers.sql
@@ -20,7 +20,7 @@ Q2 Find the name and height of the shortest player in the database. How many gam
 What is the name of the team for which he played?
 A2 Eddie Gaedel "Edward Carl" played in 1 game for SLA. His height was 43 inches.
 */
-
+--should join teams to add team name
 SELECT namegiven, height, b.teamid, b.g AS games, namefirst, namelast
 FROM people AS p
 LEFT JOIN batting AS b
@@ -67,7 +67,7 @@ FROM people AS p
 LEFT JOIN salaries AS sal
 ON p.playerid = sal.playerid
 WHERE p.playerid IN
-	(SELECT p.playerid
+	(SELECT DISTINCT(p.playerid)
 	FROM people AS p
 	LEFT JOIN collegeplaying AS cp
 	ON p.playerid = cp.playerid
@@ -122,6 +122,50 @@ ON p.playerid = f.playerid
 WHERE pos NOT IN ('P', 'C','OF', 'SS', '1B','2B','3B')
 ORDER BY position 
 
+/* 
+Q5 Find the average number of strikeouts per game by decade since 1920. 
+Round the numbers you report to 2 decimal places. Do the same for home runs per game. 
+Do you see any trends?
 
+*/
 
+--might check to see if data is same from other tables
+--need to check for trends -->excel
+SELECT DISTINCT(decade), 
+	ROUND(SUM(so) OVER(PARTITION BY decade),2) AS so_by_decade,
+	ROUND(SUM(hr) OVER(PARTITION BY decade),2) AS hr_by_decade
+FROM (SELECT so, hr,
+	  CASE WHEN yearid BETWEEN 1920 AND 1929 THEN 1920
+		   WHEN yearid BETWEEN 1930 AND 1939 THEN 1930
+	  	   WHEN yearid BETWEEN 1940 AND 1949 THEN 1940
+	       WHEN yearid BETWEEN 1950 AND 1959 THEN 1950
+	       WHEN yearid BETWEEN 1960 AND 1969 THEN 1960
+	       WHEN yearid BETWEEN 1970 AND 1979 THEN 1970
+	       WHEN yearid BETWEEN 1980 AND 1989 THEN 1980
+	       WHEN yearid BETWEEN 1990 AND 1999 THEN 1990
+	       WHEN yearid BETWEEN 2000 AND 2009 THEN 2000
+	       WHEN yearid BETWEEN 2010 AND 2019 THEN 2010
+	       ELSE 000 END AS decade
+	 FROM pitching AS p) AS sub
+ORDER BY decade DESC
+
+/*
+Q6 Find the player who had the most success stealing bases in 2016, 
+where success is measured as the percentage of stolen base attempts which are successful. 
+(A stolen base attempt results either in a stolen base or being caught stealing.) 
+Consider only players who attempted at least 20 stolen bases.
+A6 Chris Owings with 91.3 percent success.
+*/
+
+SELECT *
+FROM (SELECT namefirst , namelast , yearid,
+	  SUM(b.sb) AS successful,
+	  SUM(b.sb + b.cs) AS all_tried,
+	  ROUND(SUM(b.sb) * 100.0 / SUM(b.sb + b.cs),1) AS successful_steals
+	  FROM people AS p
+	  LEFT JOIN batting AS b
+	  ON p.playerid = b.playerid
+	  GROUP BY namefirst, namelast, yearid) AS sub
+WHERE all_tried > 19 AND yearid = 2016
+ORDER BY successful_steals DESC
 
