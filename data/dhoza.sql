778428b28a77d1af9eb6235223bc37efc7661f32
dhoza <dhoza@rocketmail.com>
2021-03-09 21:17:41 -0600

    working on Q8

--- data/dhoza.sql
+++ data/dhoza.sql
@@ -1,4 +1,4 @@
-/*Q1--A: 1871-05-04
+/*Q1 What range of years for baseball games played does the provided database cover?--A: 1871-05-04
 SELECT
 	MIN(span_first) AS first_date,
 	MAX(span_first)	AS last_date
@@ -17,9 +17,31 @@ WHERE height =
 );*/
 
 /*Q2b  How many games did he play in? 1
+
+SELECT 
+	namefirst,
+	namelast,
+	p.playerid
+	g_all
+FROM people as p
+INNER JOIN appearances as a
+ON a.playerid = p.playerid
+WHERE height =
+(
+	SELECT min(height) 
+	FROM people
+);
+
+
+
+
+
+
+
 SELECT g_all
 FROM appearances 
 WHERE playerid = 'gaedeed01';*/ 
+
 
 --Q2c  What is the name of the team for which he played? SLA
 /*SELECT teamID
@@ -96,18 +118,75 @@ ORDER BY decade;*/
 --where success is measured as the percentage of stolen base attempts which are successful. 
 --(A stolen base attempt results either in a stolen base or being caught stealing.)
 --Consider only players who attempted at least 20 stolen bases.
+--A. Chris Owings 91.30 percent success
 --GOING TO JUST USE BATTING SINCE BATTING POST TABLE NOT IN ERD AND POST MEANS OUTSIDE OF SEASON GAMES--
-WITH success_Q AS
+/*WITH success_Q AS
 (
 
-SELECT p.playerid,b.yearid,sum(cs) as caught,sum(sb) as stolen
+SELECT p.playerid,namefirst,namelast,b.yearid,sum(cs) as caught,sum(sb) as stolen
 FROM people as p
 LEFT JOIN batting as b
 ON b.playerid = p.playerid
 WHERE sb>=20 AND sb IS NOT NULL AND cs IS NOT NULL AND b.yearid >=2016
 GROUP BY b.yearid,p.playerid
 	)
-SELECT playerid,yearid,stolen,caught,(CAST(stolen as float)/(CAST(caught as float) + CAST(stolen as float))*100.0) AS percent_success FROM success_Q 
+SELECT playerid,namelast,namefirst,stolen,caught,(CAST(stolen as float)/(CAST(caught as float) + CAST(stolen as float))*100.0) AS percent_success
+
+FROM success_Q 
+ORDER BY percent_success DESC;*/
+
+--Q7a From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? A: 116
+
+SELECT teamid,yearid,wswin,w
+FROM teams
+WHERE wswin = 'N' AND yearid BETWEEN 1970 AND 2016
+ORDER BY w DESC
+LIMIT 1;
+--Q7b--What is the smallest number of wins for a team that did win the world series? A: 63
+SELECT teamid,yearid,wswin,w
+FROM teams
+WHERE wswin = 'Y' AND yearid BETWEEN 1970 AND 2016
+ORDER BY w;
+--Doing this will probably result in an unusually small number of wins for a world series champion
+--– determine why this is the case. Then redo your query, excluding the problem year.
+--How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+
+--1981 is an outlier at 63 games so remove 1981
+SELECT *
+FROM
+(
+	SELECT teamid,yearid,wswin,w
+FROM teams
+WHERE wswin = 'Y' AND yearid BETWEEN 1970 AND 2016
+ORDER BY w) AS Q
+WHERE yearid <>1981;
+
+--Q8 Using the attendance figures from the homegames table,
+--find the teams and parks which had the top 5 average attendance per game in 2016
+--(where average attendance is defined as total attendance divided by number of games).
+--Only consider parks where there were at least 10 games played.
+--Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
+SELECT park_name,h.attendance/h.games as avg_attendance
+FROM homegames AS h
+INNER JOIN parks AS p
+ON p.park = h.park
+--JOIN teams as t
+--ON h.team = t.teamid
+WHERE year = 2016 and games>=10
+ORDER BY avg_attendance desc
+
+
+SELECT park,team,attendance/games as avg_attendance
+FROM homegames
+WHERE year = 2016
+AND games>=10
+ORDER BY park
+
+
+
+
+select * from teams
+
 
 
 
