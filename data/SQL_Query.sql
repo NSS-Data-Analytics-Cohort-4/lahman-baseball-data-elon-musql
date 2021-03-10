24ad2549250fb2ce3493e2091ad2f2dc1ad07910
Engu12 <yamareworq@gmail.com>
2021-03-09 21:21:46 -0600

    updated on tuesday 03/09 but qn 9 not tried !

--- data/SQL_Query.sql
+++ data/SQL_Query.sql
@@ -7,6 +7,7 @@ SELECT COUNT(*) From salaries ;
 SELECT COUNT (Distinct playerid) FROM Salaries
 Group By playerid;
 SELECT * FROM fielding */
+SELECT * from homegames
 
 -----shortest player in the database ....
 SELECT playerid ,height
@@ -70,7 +71,7 @@ inner join TTL_sal as ts
 on vp.playerid = ts.playerid
 inner join people as p
 on vp.playerid = p.playerid
-order by ts.Total_salary desc
+order by ts.Total_salary desc;
 
 /*4.Using the fielding table, group players into three groups based on their position:
     label players with position OF as "Outfield", those with position "SS", "1B", "2B", 
@@ -93,7 +94,7 @@ ORDER BY SUM(PO);
 /*5)Find the average number of strikeouts per game by decade since 1920. Round the numbers
     you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?*/
 	
-a) for strike outs	
+--a) for strike outs	
 
 SELECT teamid,ROUND((SUM(so)+SUM(soa))/COUNT(cg),2) AS avg_strikeout_per_game,floor(Cast(yearid as int) / 10) * 10 as decade
    FROM teams AS t
@@ -101,10 +102,87 @@ SELECT teamid,ROUND((SUM(so)+SUM(soa))/COUNT(cg),2) AS avg_strikeout_per_game,fl
    GROUP BY t.teamid,decade
    ORDER BY decade;
    
- b)for home runs per game 
+-- b)for home runs per game 
 
 SELECT teamid,ROUND(SUM(hr)/COUNT(cg),2) AS avg_home_run_per_game,floor(Cast(yearid as int) / 10) * 10 as decade
    FROM teams AS t
    WHERE floor(Cast(yearid as int) / 10) * 10>=1920  
    GROUP BY t.teamid,decade
    ORDER BY decade;
+   
+/*Question 6- Find the player who had the most success stealing bases in 2016, 
+				where success is measured as the percentage of stolen base attempts which are successful. 
+				(A stolen base attempt results either in a stolen base or being caught stealing.) 
+				Consider only players who attempted at least 20 stolen bases.*/
+				
+/*SELECT playerid, round(cast(sb AS decimal)/((cast(sb as decimal) + cast(cs as decimal)),1)*100 AS stolen
+FROM batting
+WHERE yearid = '2016' AND (sb + cs) > 19
+ORDER BY stolen desc;*/
+					   
+SELECT playerid, concat((round((cast(sb as decimal)/(cast(sb as decimal) + cast(cs as decimal))),2) *100.0 ),'%') as stolen
+FROM batting
+WHERE yearid = '2016' AND (sb + cs) > 19
+ORDER BY stolen desc;
+
+					   
+--------------------Brenda's .. #6 ------------------------------------------------------------	
+SELECT *
+FROM (SELECT namefirst , namelast , yearid,
+	  SUM(b.sb) AS successful,
+	  SUM(b.sb + b.cs) AS all_tried,
+	  Concat(ROUND(SUM(b.sb) * 100.0 / SUM(b.sb + b.cs),1),'%') AS successful_steals
+	  FROM people AS p
+	  LEFT JOIN batting AS b
+	  ON p.playerid = b.playerid
+	  GROUP BY namefirst, namelast, yearid) AS sub
+WHERE all_tried > 19 AND yearid = 2016
+ORDER BY successful_steals DESC;
+					   
+---------------------------------------------------------------------------------------------------					   
+ 
+--Question 7- From 1970 – 2016, what is the largest number of wins for a team that did not win the world series?*/
+				
+SELECT teamid, yearid, w, WSWin
+FROM teams
+WHERE yearid > 1969 AND wswin = 'N'
+ORDER BY w desc
+LIMIT 1;
+
+--What is the smallest number of wins for a team that did win the world series?*/
+SELECT teamid, yearid, w, WSWin
+FROM teams
+WHERE yearid > 1969 AND wswin = 'Y'
+ORDER BY w 
+LIMIT 1;
+					   
+---------------------------------------------------------------------------------------------------------------------------------------------
+SELECT p.Park_name,hg.park, hg.team ,t.name, hg.attendance/hg.games AS Att_per_game
+FROM parks AS P
+JOIN homegames AS hg	
+ON p.park = hg.park
+JOIN teams as t
+ON hg.team = t.teamid
+WHERE year = 2016 AND games>=10
+--GROUP BY hg.team, p.park_name ,t.name
+ORDER BY Att_per_game DESC
+LIMIT 5;					   
+					   
+					   
+					   
+					   
+					   
+					   
+					   
+					   
+					   
+					   
+					   
+					   
+					   
+					   
+					   
+
+					   
+  
+   
