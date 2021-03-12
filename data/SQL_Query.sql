419a443cd1f134b852bd74ee952a4be01e7617ac
Engu12 <yamareworq@gmail.com>
2021-03-11 21:19:16 -0600

    All Questions been seen !

--- data/SQL_Query.sql
+++ data/SQL_Query.sql
@@ -9,13 +9,13 @@ Group By playerid;
 SELECT * FROM fielding */
 SELECT * from homegames
 
------shortest player in the database ....
+-----shortest player in the database---and get only the playerid & height----...
 SELECT playerid ,height
-FROM Player
+FROM People
 ORDER BY height 
 LIMIT 1;
 ---------------------
-SELECT Count(Distinct playerid) FROM people;
+SELECT Count(distinct playerid) , count(*) ,count(playerid)  FROM people;
 
 -----Q1-- The Time Range for the gaming----
 SELECT MIN(yearid) AS Starting_Year , MAX(yearid) AS Latest_Year 
@@ -96,7 +96,7 @@ ORDER BY SUM(PO);
 	
 --a) for strike outs	
 
-SELECT teamid,ROUND((SUM(so)+SUM(soa))/COUNT(cg),2) AS avg_strikeout_per_game,floor(Cast(yearid as int) / 10) * 10 as decade
+SELECT teamid,ROUND((SUM(so)+SUM(soa))/(count(cg),2) as avg_strikeout_per_game,floor(Cast(yearid as int) / 10) * 10 AS decade
    FROM teams AS t
    WHERE floor(Cast(yearid as int) / 10) * 10>=1920  
    GROUP BY t.teamid,decade
@@ -138,42 +138,177 @@ FROM (SELECT namefirst , namelast , yearid,
 	  GROUP BY namefirst, namelast, yearid) AS sub
 WHERE all_tried > 19 AND yearid = 2016
 ORDER BY successful_steals DESC;
-					   
 ---------------------------------------------------------------------------------------------------					   
- 
 --Question 7- From 1970 – 2016, what is the largest number of wins for a team that did not win the world series?*/
-				
 SELECT teamid, yearid, w, WSWin
 FROM teams
 WHERE yearid > 1969 AND wswin = 'N'
 ORDER BY w desc
 LIMIT 1;
-
 --What is the smallest number of wins for a team that did win the world series?*/
 SELECT teamid, yearid, w, WSWin
 FROM teams
 WHERE yearid > 1969 AND wswin = 'Y'
 ORDER BY w 
 LIMIT 1;
-					   
+
+SELECT COUNT(maxwin) AS mostwins_and_wonws, COUNT(wswin) AS countall_ws,
+		CONCAT(ROUND(COUNT(maxwin)*100.0/COUNT(wswin),1),'%') AS percentage_mostwins_wonws
+FROM
+	(SELECT DISTINCT(yearid), wswin,
+		CASE WHEN wswin = 'Y'  THEN MAX(w) OVER(PARTITION BY yearid) END AS maxwin
+	FROM teams
+	WHERE yearid BETWEEN 1970 AND 2016
+	GROUP BY yearid,wswin, w
+	ORDER BY yearid DESC) AS sub
+
 ---------------------------------------------------------------------------------------------------------------------------------------------
-SELECT p.Park_name,hg.park, hg.team ,t.name, hg.attendance/hg.games AS Att_per_game
+/*8.Using the attendance figures from the home games table, find the teams and parks which had the top 5 average attendance
+    per game in 2016 (where average attendance is defined as total attendance divided by number of games).
+    Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. 
+	Repeat for the lowest 5 average attendance. */
+
+--- a--The Top-5
+
+SELECT distinct p.Park_name,hg.park, hg.team,t.name, (hg.attendance/hg.games) AS Att_per_game
 FROM parks AS P
 JOIN homegames AS hg	
 ON p.park = hg.park
 JOIN teams as t
 ON hg.team = t.teamid
-WHERE year = 2016 AND games>=10
---GROUP BY hg.team, p.park_name ,t.name
+WHERE year = '2016' AND games>=10
+GROUP BY t.name, hg.team,p.park_name,hg.park,hg.attendance,t.teamid ,hg.games
 ORDER BY Att_per_game DESC
-LIMIT 5;					   
-					   
-					   
-					   
-					   
-					   
-					   
-					   
+LIMIT 5;	
+
+---b------The lowest 5----
+
+SELECT p.Park_name,hg.park,t.name,hg.team, (hg.attendance/hg.games) AS Att_per_game
+FROM parks AS P
+JOIN homegames AS hg	
+ON p.park = hg.park
+JOIN teams as t
+ON hg.team = t.teamid
+WHERE year = '2016' AND games>=10
+GROUP BY t.name, hg.team,p.park_name,hg.park,hg.attendance,hg.games
+ORDER BY Att_per_game 
+LIMIT 5;	
+	   
+-------------------------------Brenda's Approach---------------
+SELECT sub.park_name, sub.name,
+		ROUND(total_attendance_by_team /gnumber_by_team,1) AS avg_att_per_team,
+		ROUND(total_attendance_by_park /gnumber_by_park,1) AS avg_att_per_park
+FROM
+	(SELECT h.games, p.park_name, t.name,
+	 		SUM(h.attendance) OVER(PARTITION BY t.name) AS total_attendance_by_team,
+	 		SUM(h.attendance) OVER(PARTITION BY h.park) AS total_attendance_by_park,
+	 		SUM(h.games) OVER(PARTITION BY t.name) AS gnumber_by_team,
+	 		SUM(h.games) OVER(PARTITION BY h.park) AS gnumber_by_park
+	FROM homegames AS h
+	LEFT JOIN parks AS p
+	ON h.park = p.park
+	LEFT JOIN teams as t
+	ON h.team = t.teamid
+	WHERE year = 2016 AND games > 9
+	GROUP BY p.park_name, h.park, h.games, h.team, t.name, h.attendance)sub
+GROUP BY sub.park_name, sub.name, total_attendance_by_team , gnumber_by_team, total_attendance_by_park ,gnumber_by_park
+ORDER BY avg_att_per_park 
+LIMIT 5;
+
+-----------Question 9----------------------------------------------------------
+/*9.Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American 
+    League (AL)? Give their full name and the teams that they were managing when they won the award.*/
+	
+SELECT DISTINCT CONCAT(people.namefirst,' ',people.namelast) AS manager_name,awardsmanagers.lgid,
+teams.name
+FROM awardsmanagers
+INNER JOIN managers ON awardsmanagers.yearid=managers.yearid AND awardsmanagers.playerid=managers.playerid
+INNER JOIN people ON awardsmanagers.playerid=people.playerid
+INNER JOIN teams ON managers.teamid=teams.teamid
+WHERE teams.yearid>1960 AND awardsmanagers.playerid IN (SELECT distinct people.playerid FROM people
+INNER JOIN managers ON people.playerid=managers.playerid
+WHERE CONCAT(namefirst,' ',namelast) 
+IN (
+SELECT j.manager_name FROM(
+select j.yearid,j.lgid,CONCAT(j.first_name,' ',j.last_name) AS manager_name,managers.teamid
+FROM (select distinct yearid, lgid,awardsmanagers.playerid, people.namefirst AS first_name, people.namelast AS last_name
+FROM awardsmanagers
+INNER JOIN people 
+ON awardsmanagers.playerid=people.playerid
+WHERE lgid != 'ML' AND awardsmanagers.awardid='TSN Manager of the Year' AND lgid='AL'
+ORDER BY yearid) j
+INNER JOIN managers ON j.yearid=managers.yearid AND j.playerid=managers.playerid)j
+INTERSECT
+SELECT j2.manager_name FROM(
+select j.yearid,j.lgid,CONCAT(j.first_name,' ',j.last_name) AS manager_name,managers.teamid
+FROM (select distinct yearid, lgid,awardsmanagers.playerid, people.namefirst AS first_name, people.namelast AS last_name
+FROM awardsmanagers
+INNER JOIN people 
+ON awardsmanagers.playerid=people.playerid
+WHERE lgid != 'ML' AND awardsmanagers.awardid='TSN Manager of the Year' AND lgid='NL'
+ORDER BY yearid) j
+INNER JOIN managers ON j.yearid=managers.yearid AND j.playerid=managers.playerid)j2)) AND awardid='TSN Manager of the Year'
+ORDER BY manager_name
+
+--------------
+
+WITH filter_nl AS(
+		SELECT DISTINCT(am.playerid), am.yearid AS year_award_nl, t.name AS team_award_nl
+		FROM awardsmanagers AS am
+		JOIN managers AS m
+		ON am.playerid = m.playerid
+		JOIN teams AS t
+		ON m.teamid = t.teamid
+		WHERE am.awardid = 'TSN Manager of the Year' AND am.lgid = 'NL'),
+	 filter_al AS(
+		SELECT DISTINCT(am.playerid), am.yearid AS year_award_al, t.name AS team_award_al
+		FROM awardsmanagers AS am
+		JOIN managers AS m
+		ON am.playerid = m.playerid
+		JOIN teams AS t
+		ON m.teamid = t.teamid
+		WHERE am.awardid = 'TSN Manager of the Year' AND am.lgid = 'AL')
+SELECT DISTINCT(CONCAT(p.namefirst,' ', p.namelast)) AS full_name, filter_nl.year_award_nl,  filter_nl.team_award_nl, filter_al.year_award_al, filter_al.team_award_al
+FROM filter_nl
+INNER JOIN  people AS p
+ON filter_nl.playerid = p.playerid
+INNER JOIN filter_al
+ON filter_al.playerid = filter_nl.playerid
+FULL JOIN teams AS t
+ON filter_nl.team_award_nl = t.teamid
+GROUP BY full_name, filter_nl.year_award_nl,  filter_nl.team_award_nl, filter_al.year_award_al, filter_al.team_award_al
+ORDER BY full_name
+
+-----------Question 9----------------------------------------------------------
+/*9.Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American 
+    League (AL)? Give their full name and the teams that they were managing when they won the award.*/
+	
+--------------------------------------------------------------------------------------------------------------------------
+With NL As (
+           SELECT * FROM awardsmanagers 
+	       Where awardid ='TSN Manager of the Year'
+           AND lgid ='NL'),
+     AL AS (
+	       SELECT * FROM awardsmanagers
+		   Where awardid ='TSN Manager of the Year'
+		   AND lgid ='AL') 
+
+SELECT CONCAT(namefirst,' ',namelast) AS fullname ,m.teamid ,m.Playerid, am.yearid,am.lgid --t.name
+FROM people AS p
+INNER JOIN NL 
+on p.playerid=NL.playerid
+INNER JOIN AL
+ON p.playerid=AL.playerid
+INNER JOIN awardsmanagers AS am
+on p.playerid= am.playerid
+INNER JOIN managers as m
+ON am.playerid =m.playerid
+/*INNER JOIN teams t
+ON m.teamid =t.teamid */
+GROUP BY fullname,am.lgid,m.playerid,am.yearid,m.teamid ;---t.name;
+
+
+---SELECT * FROM awardsmanagers;					   
 					   
 					   
 					   
